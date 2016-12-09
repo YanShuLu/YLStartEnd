685040b166d5a6d4a2dcb81d163200383f1966fe
YanLu <yanlu198903@163.com>
2016-12-09 14:59:34 +0800

    提交

---
+++ YLStartNew/YLStartNew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLStartNew
+//
+//  Created by YanLu on 2016/12/9.
+//  Copyright © 2016年 YanLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YLStartNew/YLStartNew/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YLStartNew
+//
+//  Created by YanLu on 2016/12/9.
+//  Copyright © 2016年 YanLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
